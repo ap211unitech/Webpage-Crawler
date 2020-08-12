5512b6475360b28821d5f617d1c8273bb3f90839
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2020-08-12 11:44:01 +0530

    Final project JS file

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-let links = document.getElementsByTagName("a");
+let links = document.links;
 let fetchbtn = document.getElementById("fetchbtn");
+let list = document.getElementById("fetchlist");
 
 fetchbtn.addEventListener("click", () => {
+    list.innerHTML = "";
+    Array.from(links).forEach(elm => {
+        elm.style.color = "purple";
+    })
     let inputval = document.getElementById("fetch").value;
-    if (inputval) {
-        for (const val of links) {
-            if (val.innerText.includes(inputval)) {
-                val.style.color = "red";
-            }
+    document.getElementById("fetch").value = "";
+    Array.from(links).forEach(element => {
+        if (element.innerText.includes(inputval) && inputval != "") {
+            console.log(inputval)
+            element.style.color = "red";
+            list.innerHTML += `<li><a href=${element.href}>${element.innerText}</a></li>`;
+            console.log(list)
         }
-    }
-})
+    });
+});
 
